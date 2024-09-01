4c7dad8f72c48b0c3db450379f7ad3a5d2e88b77
Shashwat <shashwat12028@gmail.com>
2024-09-02 04:30:24 +0530

    Users Table imported

---
+++ common/tables/users.table.ts
@@ -0,0 +1,15 @@
+import { sql } from "drizzle-orm";
+import { pgTable, timestamp, varchar } from "drizzle-orm/pg-core";
+import { baseSchema } from "./base";
+
+export const usersTable = pgTable("users", {
+  ...baseSchema,
+  username: varchar("username", { length: 40 }).unique().notNull(),
+  password: varchar("password").notNull(),
+  fullName: varchar("full_name").notNull(),
+  createdAt: timestamp("created_at", { withTimeZone: true })
+    .notNull()
+    .default(sql`now()`),
+});
+export type User = typeof usersTable.$inferSelect;
+export type NewUser = typeof usersTable.$inferInsert;
