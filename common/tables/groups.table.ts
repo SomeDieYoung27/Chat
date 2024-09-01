05e82720a04c8d0373082a0a129ecc6f7fd718b6
Shashwat <shashwat12028@gmail.com>
2024-09-02 03:35:28 +0530

    Group Table created

---
+++ common/tables/groups.table.ts
@@ -0,0 +1,14 @@
+import { bigint, pgTable, varchar } from "drizzle-orm/pg-core";
+import { baseSchema } from "./base";
+import { usersTable } from "./users.table";
+
+export const groupsTable = pgTable("groups", {
+  ...baseSchema,
+  name: varchar("name", { length: 50 }).notNull(),
+  ownerId: bigint("owner_id", { mode: "number" })
+    .notNull()
+    .references(() => usersTable.id),
+});
+
+export type Group = typeof groupsTable.$inferSelect;
+export type NewGroup = typeof groupsTable.$inferInsert;
