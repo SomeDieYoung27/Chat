c3d78e3aa071ff4443f865d6220262add6640a32
Shashwat <shashwat12028@gmail.com>
2024-09-17 02:41:12 +0530

    Config in server added

---
+++ server/src/config.ts
@@ -0,0 +1,17 @@
+import "dotenv/config";
+
+export const config = {
+  appName: "Seri",
+  port: process.env.PORT || 5000,
+  jwtKey: "jwt",
+  accessTokenExpiry: "15m",
+  refreshTokenExpiry: "7d",
+  refreshTokenMaxAge: 7 * 24 * 60 * 60 * 1000,
+  accessTokenSecret: process.env.ACCESS_TOKEN_SECRET || "complexsecret",
+  refreshTokenSecret: process.env.REFRESH_TOKEN_SECRET || "morecomplexsecret",
+  dbUrl: process.env.DB_URL!,
+  corsOrigin: process.env.CORS_ORIGIN || "http://localhost:3000",
+  isProd: process.env.NODE_ENV === "production",
+  redisHost: process.env.REDIS_HOST || "localhost",
+  redisPort: process.env.REDIS_PORT || 6379,
+};
